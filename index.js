03cb24223b009200a7ade10f79e77b50b5d51534
cesar mayta <cesarmayta@gmail.com>
2022-03-09 11:54:45 -0500

    d1

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+/************************LEVANTAR SERVIDOR POR PUERTO 3000****************/
+app.set('port',process.env.PORT || 5000);
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname,'public')));
+
+
+const server = app.listen(app.get('port'),() =>{
+    console.log(`servidor : http://localhost:${app.get("port")}`);
+})
+/**********************************************************************/
+
+//creación de socket
+const SocketIO = require('socket.io');
+
+const io = SocketIO(server);
+
+io.on('connection',(socket) =>{
+    console.log("nueva conexion",socket.id);
+
+    socket.on('mensajecliente',(data)=>{
+        console.log(data);
+        io.sockets.emit('mensajeservidor',data);
+    });
+
+});
+
